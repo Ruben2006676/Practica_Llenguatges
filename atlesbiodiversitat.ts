e1c8e760c67e75933f9af0550d8d14764ce983b7
Ruben2006676 <rubenlagolopez2006@gmail.com>
2025-03-11 10:01:36 +0100

    Add files via upload

---
+++ atlesbiodiversitat.ts
@@ -0,0 +1,10 @@
+export class biodiversitat{
+    especie_id : string
+    nom_ctf: string
+    nom_cat: string
+    nom_cas: string
+    nom_eng: string
+    desc_cat: string
+    desc_cas: string
+    desc_eng: string
+}
