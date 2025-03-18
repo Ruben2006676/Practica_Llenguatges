9bb3ac3d2519b5ea56d00d02c93aaa1759c672f3
Rubén Lago <rubenlagolopez2006@gmail.com>
2025-03-18 08:42:25 +0100

    h

--- validacio_biodiversitat.ts
+++ validacio_biodiversitat.ts
@@ -1,10 +1,10 @@
-import Ajv from "ajv"; // Importa la librería Ajv para validar datos JSON contra esquemas.
-import { readFile } from "fs/promises"; // Importa el método readFile para leer archivos de manera asincrónica.
-import type { biodiversitat } from "./atlesbiodiversitat"; // Importa el tipo 'biodiversitat' desde otro archivo para tipar los datos que se van a manejar.
+import Ajv from "ajv"; // Importa la llibreria Ajv para validar dades JSON.
+import { readFile } from "fs/promises"; // Importa el mètode readFile per llegir arxius.
+import type { biodiversitat } from "./atlesbiodiversitat"; // Importa el tipus 'biodiversitat' des d'un altre arxiu per tipar les dades que s'utilitzaran.
 import bioSchema from "./atlesbiodiversitat.json"; // Importa el archivo JSON que probablemente contiene el esquema de validación.
 
-const data = await readFile('./atlesbiodiversitat_vw_flora_especies.json', 'utf-8'); // Lee el archivo JSON con los datos de las especies de flora.
-const bios: biodiversitat[] = await JSON.parse(data); // Convierte el archivo JSON leído en un array de objetos 'biodiversitat'.
+const data = await readFile('./atlesbiodiversitat_vw_flora_especies.json', 'utf-8'); // Llegeix JSON amb les dades específiques de biodiversitat.
+const bios: biodiversitat[] = await JSON.parse(data); // Converteix l'arxiu JSON llegit en un array d'objectes 'biodiversitat'.
 
 
 //Creem constants per crear un Ajv y un validador que compili el JSON amb JSONSchema
